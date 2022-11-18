c248c58f5234ba6e199343e11b128aaaa52fa69b
xiachenchen <2539177130@qq.com>
2022-11-18 14:05:56 +0800

    第一次提交

---
+++ src/main/java/com/xydz/suppliermateriallabelprintingtool/SupplierMaterialLabelPrintingToolApplication.java
@@ -0,0 +1,13 @@
+package com.xydz.suppliermateriallabelprintingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierMaterialLabelPrintingToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierMaterialLabelPrintingToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xydz/suppliermateriallabelprintingtool/SupplierMaterialLabelPrintingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xydz.suppliermateriallabelprintingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierMaterialLabelPrintingToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
